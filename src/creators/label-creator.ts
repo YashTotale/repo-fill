19ed132711c402f9a7750e029f082e406c4f6cb3
YashTotale <totaleyash@gmail.com>
2021-01-15 14:26:17 -0800

    Refactored label-creator

--- src/creators/label-creator.ts
+++ src/creators/label-creator.ts
@@ -12,26 +12,37 @@ export type RepoLabels = RestEndpointMethodTypes["issues"]["getLabel"]["response
 export const REPO_LABELS = (repo: Repo) =>
   `repo-labels/${checkOrg(repo)}${repo.name}.json`;
 
+interface Label {
+  description: string;
+  color: string;
+}
+
+export const REQUIRED_LABELS: Record<string, Label> = {
+  stale: {
+    description: "No activity",
+    color: "ebdcb5",
+  },
+  "feature-request": {
+    description: "New feature",
+    color: "340EDA",
+  },
+};
+
 export const getRepoLabels = async (
   repo: Repo,
   cache: Cache
 ): Promise<RepoLabels> => {
   const cachedLabelsFile = cache[REPO_LABELS(repo)];
 
-  let repoLabelContents;
-
-  if (typeof cachedLabelsFile === "string")
-    repoLabelContents = JSON.parse(cachedLabelsFile);
+  if (typeof cachedLabelsFile === "string") return JSON.parse(cachedLabelsFile);
   else {
     console.log(`Getting repo '${repo.name}' labels...`);
     const { data: repoLabels } = await axiosGet(
       repo.labels_url.replace("{/name}", "")
     );
-    repoLabelContents = repoLabels;
     writeToCache(REPO_LABELS(repo), JSON.stringify(repoLabels));
+    return repoLabels;
   }
-
-  return repoLabelContents;
 };
 
 export const createLabels = async (
@@ -40,20 +51,9 @@ export const createLabels = async (
   user: User,
   repoLabelContents: RepoLabels
 ) => {
-  const requiredLabels = {
-    stale: {
-      description: "No activity",
-      color: "ebdcb5",
-    },
-    "feature-request": {
-      description: "New feature",
-      color: "340EDA",
-    },
-  };
-
-  for (const label in requiredLabels) {
+  for (const label in REQUIRED_LABELS) {
     const found = repoLabelContents.find((l) => l.name === label);
-    const properties = requiredLabels[label];
+    const properties = REQUIRED_LABELS[label];
 
     if (!found) {
       console.log(`Creating label '${label}'...`);
